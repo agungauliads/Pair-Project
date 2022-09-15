88c6255bb3f1debe53c5a23fa5a47db9f364610e
Agung Aulia <agungauliads@gmail.com>
2022-09-15 15:44:07 +0700

    auth done

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
 const express = require('express')
+const session = require('express-session')
 const Controller = require('./controllers/index')
 const app = express()
 const port = 3000
+const {isLogin, isModerator} = require('./middlewares/authorize')
 
+app.use(
+    session({
+        secret: "secret",
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            secure: false,
+            sameSite: true
+        }
+    })
+);
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+
 
 app.get('/', Controller.home)
 app.get('/register', Controller.formRegister)
 app.post('/register', Controller.handleRegister)
 app.get('/login', Controller.formLogin)
 app.post('/login', Controller.handleLogin)
-
+app.get('/logout', Controller.logout)
+app.use(isLogin)
+app.use(isModerator)
+app.get('/index', Controller.index)
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Pengguna Pinterin sebanyak ${port} User`)
 })
--- controllers/index.js
+++ controllers/index.js
@@ -1,59 +1,78 @@
-const {User, Profile, Post} = require("../models/index")
+const { User, Profile, Post } = require("../models/index")
 const bcryptjs = require('bcryptjs')
 
-class Controller{
-    static home(req, res){
+class Controller {
+    static home(req, res) {
         res.render('home')
     }
 
-    static formRegister(req, res){
+    static formRegister(req, res) {
         res.render('formRegister')
     }
 
-    static handleRegister(req, res){
-        const {username, email, password, role} = req.body
+    static handleRegister(req, res) {
+        const { username, email, password, role } = req.body
         User.create({
             username,
             email,
             password,
             role
         })
-        .then(data => {
-            res.redirect('/login')
-        })
-        .catch(err => {
-            res.send(err)
-        })
+            .then(data => {
+                res.redirect('/login')
+            })
+            .catch(err => {
+                res.send(err)
+            })
     }
 
-    static formLogin(req, res){
-        res.render('formLogin')
+    static formLogin(req, res) {
+        const { error } = req.query
+        res.render('formLogin', {error})
     }
 
-    static handleLogin(req, res){
+    static handleLogin(req, res) {
         const { username, password } = req.body
-        User.findByPk({
+        User.findOne({
             where: {
                 username
             }
         })
-        .then(data => {
-            if(data){
-                const isValidPassword = bcryptjs.compareSync(password, data.password)
-                if(isValidPassword){
-                    return res.redirect('/')
-                }else{
-                    const error = "invalid username/password"
+            .then(data => {
+                // console.log(data);
+                if (data) {
+                    const isValidPassword = bcryptjs.compareSync(password, data.password)
+                    if (isValidPassword) {
+                        req.session.user = {id: data.id, role: data.role}
+                        // req.session.role = data.role
+                        return res.redirect('/')
+                    } else {
+                        const error = "invalid username / password"
+                        return res.redirect(`/login?error=${error}`)
+                    }
+                } else {
+                    const error = "invalid username / password"
                     return res.redirect(`/login?error=${error}`)
-                }   
+                }
+            })
+            .catch(err => {
+                // console.log(err);
+                res.send(err)
+            })
+    }
+
+    static logout(req, res){
+        req.session.destroy((err) => {
+            if(err){
+                res.send(err);
             }else{
-                const error = "invalid username/password"
-                    return res.redirect(`/login?error=${error}`)
+                res.redirect('/login')
             }
         })
-        .catch(err => {
-            res.send(err)
-        })
+    }
+
+    static index(req, res){
+        res.render('index')
     }
 }
 
